3f77b14194d82fa274927a817a3707d9c28cc886
Mafinar Khan <mafinar@gmail.com>
2018-03-10 00:23:00 -0500

    P31

--- lib/ninety_nine/arithmetic.ex
+++ lib/ninety_nine/arithmetic.ex
@@ -4,8 +4,25 @@ defmodule NinetyNine.Arithmetic do
   """
   @doc """
   [P31] Determine whether a given integer number is prime.
+
+  ## Usage
+
+      iex> import NinetyNine.Arithmetic
+      iex> prime?(7879)
+      true
+      iex> prime?(9)
+      false
   """
-  def prime?(_), do: :"TODO:"
+  def prime?(n) when n <= 0, do: false
+  def prime?(n) when 0 < n and n <= 3, do: true
+
+  def prime?(n) when rem(n, 2) == 0, do: false
+
+  def prime?(n) do
+    not (2..(n - 1)
+         |> Enum.filter(&(rem(n, &1) == 0))
+         |> Enum.any?())
+  end
 
   @doc """
   [P32] Determine the greatest common divisor of two positive integer numbers.
@@ -17,4 +34,14 @@ defmodule NinetyNine.Arithmetic do
   """
   def gcd(x, 0), do: x
   def gcd(x, y), do: gcd(y, rem(x, y))
+
+  @doc """
+  [P33] Determine whether two positive integer numbers are coprime.
+
+  ## Usage
+      iex> import NinetyNine.Arithmetic
+      iex> coprime?(35, 64)
+      true
+  """
+  def coprime?(x, y), do: gcd(x, y) == 1
 end

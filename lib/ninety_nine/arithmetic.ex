0d7e84cfecece04da02dae0310bb32bdb3c239f9
Mafinar Khan <mafinar@gmail.com>
2018-03-10 00:04:30 -0500

    P32

--- lib/ninety_nine/arithmetic.ex
+++ lib/ninety_nine/arithmetic.ex
@@ -6,4 +6,15 @@ defmodule NinetyNine.Arithmetic do
   [P31] Determine whether a given integer number is prime.
   """
   def prime?(_), do: :"TODO:"
+
+  @doc """
+  [P32] Determine the greatest common divisor of two positive integer numbers.
+
+  ## Usage
+      iex> import NinetyNine.Arithmetic
+      iex> gcd(36, 63)
+      9
+  """
+  def gcd(x, 0), do: x
+  def gcd(x, y), do: gcd(y, rem(x, y))
 end

90969451c4441338119532b794e56d9629e0e11a
Mafinar Khan <mafinar@gmail.com>
2018-03-09 23:23:48 -0500

    Moduledocs for the math functions

--- lib/ninety_nine/arithmetic.ex
+++ lib/ninety_nine/arithmetic.ex
@@ -1,4 +1,7 @@
 defmodule NinetyNine.Arithmetic do
+  @moduledoc """
+  Solve the Arithmetic problems (31..41)
+  """
   @doc """
   [P31] Determine whether a given integer number is prime.
   """
--- lib/ninety_nine/math_utils.ex
+++ lib/ninety_nine/math_utils.ex
@@ -1,2 +1,5 @@
 defmodule NinetyNine.MathUtils do
+  @moduledoc """
+  Utilities for computing math functions
+  """
 end
